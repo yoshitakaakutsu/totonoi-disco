b5b95557a70a1def729b708084f92039ffaee172
EC2 Default User <ec2-user@ip-172-31-80-193.ec2.internal>
2022-07-06 05:06:03 +0000

    投稿機能追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -7,8 +7,9 @@ class Public::PostsController < ApplicationController
   end
   
   def  create
-    post = Post.new(post_params)
-    post.save
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
     redirect_to root_path
   end
   
