3062e8a90fc4ddf410cbf6367eea7eab0e682b33
yoshitakaakutsu <lf4506@gmail.com>
2022-10-24 12:21:02 +0000

    [fix] fix assets path

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,17 +2,19 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "jquery";
-import "popper.js";
-import "bootstrap";
-import "../stylesheets/application" 
+import Jquery from "jquery";
+import Popper from "popper.js";
+import Bootstrap from "bootstrap";
 import '@fortawesome/fontawesome-free/js/all'
+import "stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
